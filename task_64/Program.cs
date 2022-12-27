234913b91fd41ccd1b48cdde3cde382cbea2eabf
OlikNN <oliknn@gmail.com>
2022-12-28 01:39:32 +0300

    init

---
+++ task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Numbers(int number) 
+{
+    Console.Write(number + " ");
+    
+    if (number <= 1 )
+    {
+        return;
+    }
+    
+    Numbers(number - 1);
+}
+
+Numbers(n);
+
---
+++ task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Numbers(int m, int n, int sum)
+{
+    if (n < m) return;
+    sum += n;
+    Console.WriteLine($"m={m} n={n} sum={sum}");
+    Numbers(m, n - 1, sum);
+}
+
+Numbers(m, n, 0);
+
